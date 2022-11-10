c0c8af75be4863909fb6bbe4779384cebd10041d
Long Nguyen-Vu <nve164@gmail.com>
2022-11-10 15:04:35 +0900

    fix the learning rate and batchsize for a lot more stable training, turn off writing log for tensorboard to save GPU memory

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -111,8 +111,8 @@ class Trainer(BaseTrainer):
                 # self.writer.add_image('input', make_grid(data.cpu(), nrow=8, normalize=True))
 
         # add histogram of model parameters to the tensorboard
-        for name, p in self.model.named_parameters():
-            self.writer.add_histogram(name, p, bins='auto')
+        # for name, p in self.model.named_parameters():
+        #     self.writer.add_histogram(name, p, bins='auto')
 
         return {
             'val_loss': total_val_loss / len(self.valid_data_loader),
